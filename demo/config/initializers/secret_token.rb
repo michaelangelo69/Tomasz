909517454719a82cbebe3eb9d3b0d550bc3d1599
Magdalena Orłowska <Magdalena.Orlowska@dreamlab.pl>
2017-01-21 01:17:19 +0100

    Aplikacja demonstracyjna, wrzucenie layoutów + baza danych.

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '1856703cbfd5842a12db03dfe99835a74763c4bbaa2e60a0d74ac6082775e54df3f2fac110a0994d10a12e4463d8a670cbac420d8322475901d640b80830437f'
